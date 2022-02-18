c4877a7afaed47193f79b03cd60ea0728307dc2e
devanessa96 <99607955+devanessa96@users.noreply.github.com>
2022-02-18 09:50:38 -0300

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,17 @@
+// Exercício IF ELSE e Operações Aritméticas
+
+const nota1 =  2;
+const nota2 = 10;
+
+if (nota1+nota2/2 >= 8)  { 
+       console.log("Aprovado");
+
+ }  else  if (nota1+nota2/2 == 7) {
+ 
+    console.log("Reprovado")
+ }
+ else
+ {
+
+   console.log("Abaixo da média"); 
+ }
